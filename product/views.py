39ff297f78279ec9c29770ce8cc29be36b5601df
dayeon-21 <ldy21@hanyang.ac.kr>
2025-01-14 20:17:38 +0900

    3주차 django 개발 완료

--- product/views.py
+++ product/views.py
@@ -1,6 +1,6 @@
 # from django.http import HttpResponse
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import MainContent
 
 def index(request):
@@ -8,3 +8,7 @@ def index(request):
     context = {'content_list': content_list}
     return render(request,'product/content_list.html', context)
 
+def detail(request, content_id):
+    content_list = get_object_or_404(MainContent, pk=content_id)
+    context = {'content_list': content_list}
+    return render(request,'product/content_detail.html', context)
